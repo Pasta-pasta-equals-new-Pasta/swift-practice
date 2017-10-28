938c5ccf96d28c148fdc8bc4355b433a40df1445
Steven <linguinealfredo@gmail.com>
2017-10-28 14:18:42 -0700

    Initial commit

---
+++ swift-practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-practice
+//
+//  Created by UCode on 10/28/17.
+//  Copyright © 2017 Your name in here. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
